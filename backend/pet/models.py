b4f31a8d43c1a34e2dc03ce877c1e2a8af3745ab
Jerry-LIn <jarry0905337377@gmail.com>
2024-05-12 21:53:09 +0800

    first commit

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from user.models import Users
 # Create your models here.
+
+class Pet(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='pets')
+    name = models.CharField(max_length=20)
+    breed = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField()
