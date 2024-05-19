1376ccfe0ea92c79e87917d951b87347e5497002
Seco1024 <deyenqiu@gmail.com>
2024-05-19 13:03:23 +0800

    Remove __pycache__ directories from version control

--- backend/user/models.py
+++ backend/user/models.py
@@ -6,11 +6,11 @@ import uuid
 # Create your models here.
 class Profile(models.Model):
     # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(max_length = 50, null = False, blank = True)
+    email = models.EmailField(max_length = 60, null = False, blank = True)
     username = models.CharField(max_length = 20, null = False, blank = True)
     password = models.CharField(max_length = 20, null = False, blank = True)
     phone = models.TextField(max_length = 20, null = False, blank = False)
-    uid = models.UUIDField(primary_key= True,default=uuid.uuid4, editable=False, unique=True)
+    uid = models.UUIDField(primary_key= True, default=uuid.uuid4, editable=False, unique=True)
     date = models.DateTimeField(default=timezone.now)
 
     
