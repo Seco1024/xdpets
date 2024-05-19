4b7522c5f96e43f8158e9bd0585c1af0df5fe86a
Seco1024 <deyenqiu@gmail.com>
2024-05-19 17:17:32 +0800

    fix: pet model rename

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -5,9 +5,9 @@ import uuid
 # Create your models here.
 
 class Pet(models.Model):
-    petId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    ownerId = models.ForeignKey(Profile, on_delete=models.CASCADE, db_column='uid')
-    name = models.CharField(max_length=20)
+    pet_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    owner_id = models.ForeignKey(Profile, on_delete=models.CASCADE, db_column='uid')
+    pet_name = models.CharField(max_length=20)
     breed = models.CharField(max_length=15, blank=True, null=True)
     category = models.CharField(max_length=15, blank=True, null=True)
     gender = models.CharField(max_length=10, blank=True, null=True)  
