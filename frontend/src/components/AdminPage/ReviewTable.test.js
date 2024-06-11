0a08fb59eb9fdcd6b7d3d1661c2043fdd0f3a3cc
liuh2so4 <xuanyu60413@gmail.com>
2024-06-12 06:26:52 +0800

    update ReviewTable.test

--- frontend/src/components/AdminPage/ReviewTable.test.js
+++ frontend/src/components/AdminPage/ReviewTable.test.js
@@ -3,7 +3,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import SentAdoptionInfoTable from '../SentAdoptionInfoPage/sentAdoptionInfoTable';
 import axios from 'axios';
 import ConfirmationDialog from '../ConfirmationDialog';
-import DetailsDialog from './DetailsDialog';
+import DetailsDialog from './DetailDialog';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 
