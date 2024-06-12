a69cf4d36769ac6f09c8fea925cbe8a55dd41458
ocar1053 <64206644+ocar1053@users.noreply.github.com>
2024-06-12 15:06:03 +0800

    Revert "Merge branch 'Test' into 'BackEnd'"
    This reverts commit b49ce46ec796981043e3e6628a79280b7ac7ffb2, reversing
    changes made to a356a0d0ef963fbca32a62f6a4eeff20ece6db54.

--- frontend/src/components/MainPage/CardArea.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import CardArea from './CardArea';
-
-test('renders CardArea component', () => {
-  render(<CardArea />);
-  // This line is only to make sure the component renders without throwing errors
-  expect(screen.getByText('搜尋')).toBeInTheDocument();
-});
